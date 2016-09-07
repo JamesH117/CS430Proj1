4dcf1af7970e2f89210866c7b536e972be15465f
JamesH117 <level.5.misaka@gmail.com>
2016-09-07 11:46:10 -0700

    Add files via upload
    Just creating temporary main file with idea text file on how to do project

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    //Grab variable that is output type
+    output_type = argv[1];
+
+    //Grab input file to convert
+    input_file = argv[2];
+
+
+    //output_file = argv[3];
+    return 0;
+}
