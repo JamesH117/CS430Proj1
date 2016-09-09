5ff2875a174ba341c4cfaa4961e507757f75b763
JamesH117 <level.5.misaka@gmail.com>
2016-09-09 16:58:46 -0700

    Read is 80% Write is 80%
    Read function works on P3 files, not yet P6 files.  Write currently
    writes all important data into file.  Need to work on convert function
    to change ASCII to Raw Bits and Raw Bits to ASCII and then write is
    complete.

--- main.c
+++ main.c
@@ -11,30 +11,38 @@ typedef struct PPMimage {
     PPMpixel *buffer;
     } PPMimage;
 
+PPMimage *image;
 
-//Does not work for P6 yet
-int ppm_read(char *output_type, char *input_file, char *output_file){
-    //If output_type is 6 creat a wb file
-    //If input file is P6 open a rb file
-    FILE *fh = fopen(input_file, "r");
-    FILE *fp = fopen(output_file, "w");
+//Function to convert ASCII to Binary Bits and Binary Bits to ASCII
+int ppm_convert(int output_type){
+return 0;
+}
 
+//Does not work for P6 yet, put catcher so you open a Binary file if given P6 File
+int ppm_read(int output_type, char *input_file){
+    FILE *fh;
     int max_color, width, height;
     int c;
-    PPMimage *image;
+    //If output_type is 6 creat a wb file
+    //If input file is P6 open a rb file
+    if(output_type == 3){
+        fh = fopen(input_file, "r");
+        //printf("Opening P3");
 
-    //Open file to read into buffer
+    }
+    if(output_type == 6){
+        fh = fopen(input_file, "rb");
+        //printf("Opening P6");
+    }
+
+
 
     //If fh == 0 then file did not open, error out
     if(fh == 0){
         fprintf(stderr, "Error: Unable to open file '%s' \n", input_file);
         return 1;
     }
-    //If fh == 0 then file did not create, error out
-    if(fp == 0){
-        fprintf(stderr, "Error: Unable to create file '%s' \n", output_file);
-        return 1;
-    }
+
 
     //Check the first character of the opened file to make sure it is a PPM file, otherwise error out
     c = fgetc(fh);
@@ -88,17 +96,56 @@ int ppm_read(char *output_type, char *input_file, char *output_file){
     image->buffer = (PPMpixel*)malloc(sizeof(PPMpixel) * width * height);
     //This code writes to the buffer of image to transfer to output file
     fread(image->buffer, 3*width, height, fh);
-    //This code writes to output image, but only the pixels
-    fwrite(image->buffer, 3*width, height, fp);
-
-
-
-    fclose(fp);
     fclose(fh);
     return 0;
 }
 
-int ppm_write(){
+int ppm_write(int output_type, char *output_file){
+    FILE *fp;
+    int width, height;
+    int c;
+    char magic_number[2];
+    width = image->width;
+    height = image->height;
+    magic_number[0] = 'P';
+
+    if(output_type == 3){
+        magic_number[1] = '3';
+    }else{
+        magic_number[1] = '6';
+    }
+    //printf("%c%c", magic_number[0], magic_number[1]);
+
+
+    //Open binary file to write to if 6 is given
+    if(output_type == 3){
+        fp = fopen(output_file, "w");
+        //printf("Opening P3 to write to. \n");
+
+    }
+    if(output_type == 6){
+        fp = fopen(output_file, "wb");
+        //printf("Opening P6 to write to. \n");
+    }
+
+    //If fp == 0 then file did not create, error out
+    if(fp == 0){
+        fprintf(stderr, "Error: Unable to create file '%s' \n", output_file);
+        return 1;
+    }
+    //Write appropriate magic number to file based on output_type
+    fwrite(magic_number, sizeof(magic_number), sizeof(magic_number)-1, fp);
+    //Write image dimensions next
+    fprintf(fp,"\n%d %d", width, height);
+    //Write max colors (always 255)
+    fprintf(fp,"\n255");
+
+    //Call Function Here to convert image->buffer to either ASCII or Raw Bits
+
+    //This code writes to output image, but only the pixels
+    fwrite(image->buffer, 3*width, height, fp);
+    fclose(fp);
+    return 0;
 }
 
 int main(int argc, char *argv[]){
@@ -108,6 +155,8 @@ int main(int argc, char *argv[]){
 
     //Grab variable that is output type
     char *output_type = argv[1];
+    int type = atoi(output_type);
+    //printf("%d", type);
     //Put Error checker to make sure output_type is 3 or 6.
 
     //Grab input file to convert
@@ -116,6 +165,7 @@ int main(int argc, char *argv[]){
     //Grab name of output file to create
     char *output_file = argv[3];
 
-    ppm_read(output_type, input_file, output_file);
+    ppm_read(type, input_file);
+    ppm_write(type, output_file);
     return 0;
 }
